1200f25a96a5ecde1572596fd1cf9ff5373ccca8
taiwo2 <adetonat@ymail.com>
2021-04-01 00:55:33 +0100

    Fix my restuarant app

---
+++ src/main-home.js
@@ -0,0 +1,32 @@
+import { $content, createHtmlElement } from "./index";
+
+function render() {
+  const $main = document.createElement("main");
+
+  const $bienvenidos = createHtmlElement(
+    "p",
+    null,
+    ["cursive", "golden"],
+    "Tonaz Restuarant"
+  );
+  const $title = createHtmlElement("h1", null, ["white"], "Home of Foods");
+
+  const $hr = document.createElement("hr");
+  const $subtitle = createHtmlElement(
+    "p",
+    null,
+    ["text-center", "white"],
+    "A Place to enjoy Good food that satisfy Heart!"
+  );
+  const $button = createHtmlElement("button", null, null, "HERE FOR MENU");
+
+  $main.appendChild($bienvenidos);
+  $main.appendChild($title);
+  $main.appendChild($hr);
+  $main.appendChild($subtitle);
+  $main.appendChild($button);
+
+  $content.appendChild($main);
+}
+
+export { render as renderHome };
